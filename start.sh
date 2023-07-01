f3bceeb28a2fc29feb2b624aefca701351a9f63e
thewislight <89086788+thewislight@users.noreply.github.com>
2023-07-01 09:23:49 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ CONFIGCADDY=https://raw.githubusercontent.com/thewislight/srallhandle/master/etc
 CONFIGXRAY=https://raw.githubusercontent.com/thewislight/srallhandle/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
 StoreFiles=https://raw.githubusercontent.com/thewislight/srallhandle/master/etc/StoreFiles
-#PORT=4433
+#PORT=4435
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
